71a15e33504e37d4bcf668a885bfbec5155a70a0
hypnoSpirA7 <hypnoSpirA7@users.noreply.github.com>
2020-07-18 01:38:23 +0800

    unity chan V0.1

---
+++ CSharp_API_IF/Assets/Scripts/UnityChan.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Permissions;
+using UnityEngine;
+
+public class Unity_Chan : MonoBehaviour
+{
+ 
+   private void Walk()
+    {
+        bool key = Input.GetKey(KeyCode.W);
+
+        ani.Setbool ("走路開關", key);
+    }
+
+   private void Update()
+    {
+        Walk();
+    }
+}
